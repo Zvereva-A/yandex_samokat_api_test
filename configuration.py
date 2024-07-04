28dd32a3c57f07d3c4b46b445f2ebb64e9fc4fe7
Zvereva Anastasiya <akuritsyna751@gmail.com>
2024-07-04 16:51:13 +0300

    add screenshot, add sql requests

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://54350e13-ebd9-4d74-982f-0138568e91ae.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://3ee8f7fe-845b-41b2-8dbe-8c9f24b56aa4.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_BY_TRACK = "/api/v1/orders/track"
--- get_order_by_track.py
+++ get_order_by_track.py
@@ -1,6 +1,6 @@
 import sender_stand_request
 
-
+# Анастасия Зверева, 18-я когорта — Финальный проект. Инженер по тестированию плюс
 def test_get_order_by_track():
     response = sender_stand_request.get_order_by_track()
 
